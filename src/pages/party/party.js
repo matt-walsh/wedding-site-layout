41799a01bf83ea4476aaab5ebde72dfa84865784
matt.walsh.dev@gmail.com <matt.walsh.dev@gmail.com>
2018-04-08 09:55:50 -0300

    added custom fonts

--- src/pages/party/party.js
+++ src/pages/party/party.js
@@ -29,84 +29,86 @@ export class Party extends React.Component {
         <div className="groom-container">
           <div className="mark-container">
             <img src={Mark} alt="Mark"/>
-            <h2>Best Man</h2>
+            <h3>Best Man</h3>
           </div>
           <div className="griff-container">
             <img src={Griffin} alt="Griff"/>
-            <h2>Friend</h2>
+            <h3>Friend</h3>
           </div>
           <div className="ryan-container">
             <img src={Ryan} alt="Ryan"/>
-            <h2>Friend</h2>
+            <h3>Friend</h3>
           </div>
           <div className="will-container">
             <img src={Will} alt="Will"/>
-            <h2>Friend</h2>
+            <h3>Friend</h3>
           </div>
           <div className="jordan-container">
             <img src={Jordan} alt="Jordan"/>
-            <h2>Nephew</h2>
+            <h3>Nephew</h3>
           </div>
           <div className="walshs-container">
             <img src={Walshs} alt="Parents"/>
-            <h2>Mom &amp; Dad</h2>
+            <h3>Mom &amp; Dad</h3>
           </div>         
         </div>
 
         <div className="bride-container">
           <div className="tiff-container">
             <img src={Tiffany} alt="Tiffany"/>
-            <h2>Maid of Honor</h2>
+            <h3>Maid of Honor</h3>
           </div>
 
           <div className="vanessa-container">
             <img src={Vanessa} alt="Vanessa"/>
-            <h2>Sister</h2>
+            <h3>Sister</h3>
           </div>
 
           <div className="megan-container">
             <img src={Megan} alt="Megan"/>
-            <h2>Sister</h2>
+            <h3>Sister</h3>
           </div>
           
           <div className="jen-container">
             <img src={Jen} alt="Jen"/>
-            <h2>Sister in Law</h2>
+            <h3>Sister in Law</h3>
           </div>
 
           <div className="dan-container">
             <img src={Dan} alt="Dan"/>
-            <h2>Friend</h2>
+            <h3>Friend</h3>
           </div>
 
           <div className="debra-container">
             <img src={Debra} alt="Debra"/>
-            <h2>Mom</h2>
+            <h3>Mom</h3>
           </div>                
         </div>
 
         <div className="performers-container">
           <div className="oliver-container">
             <img src={Oliver} alt="Oliver"/>
-            <h2>Ring Bearer</h2>
+            <h3>Ring Bearer</h3>
           </div>
           <div className="sadie-container">
             <img src={Sadie} alt="Sadie"/>
-            <h2>Flower Girl</h2>
+            <h3>Flower Girl</h3>
           </div>
         </div>
 
-        <div className="dona-container">
-          <img src={Dona} alt="Dona"/>
-          <h2>Photographer</h2>
-        </div>
-        <div className="fiona-container">
-          <img src={Fiona} alt="Fiona"/>
-          <h2>Master of Ceremony</h2>
-        </div>
-        <div className="chris-container">
-          <img src={Chris} alt="Chris"/>
-          <h2>Bar Tender</h2>
+        <div className="staff-container">
+          <div className="dona-container">
+            <img src={Dona} alt="Dona"/>
+            <h3>Photographer</h3>
+          </div>
+          <div className="fiona-container">
+            <img src={Fiona} alt="Fiona"/>
+            <h3>Master of Ceremony</h3>
+          </div>
+          <div className="chris-container">
+            <img src={Chris} alt="Chris"/>
+            <h3>Bar Tender</h3>
+          </div>
         </div> 
       </div>
     );
